5b6d7e3cdeadb167beeeafd5faf08420996997a9
sebastian171000 <67174462+sebastian171000@users.noreply.github.com>
2020-11-09 23:52:19 -0500

    Update HomeController.java

--- src/main/java/com/eparking/springboot/app/controllers/HomeController.java
+++ src/main/java/com/eparking/springboot/app/controllers/HomeController.java
@@ -46,15 +46,59 @@ public class HomeController {
 	
 	@PostMapping("/espacios-disponibles/update")
 	public String espaciosDisponiblesUpdate(@ModelAttribute("usuarioSesion") Usuario usuario,
-			@RequestParam(name = "espaciosD") int espaciosD, RedirectAttributes flash) {
+			@RequestParam(name = "espaciosD") int espaciosD,@RequestParam(name="anticipacionDias") int anticipacionDias,
+			@RequestParam(name="anticipacionHoras", required = false) int anticipacionHoras,
+			@RequestParam(name = "horaApertura") String horaApertura,
+			@RequestParam(name = "horaCierre") String horaCierre,
+	RedirectAttributes flash) {
 		Estacionamiento estacionamiento = new Estacionamiento();
 		estacionamiento = esService.findByUser(usuario);
-		if(espaciosD>0 && espaciosD < estacionamiento.getEspacios()) {
+		
+		if(espaciosD>0 && espaciosD <= estacionamiento.getEspacios()) {
 			estacionamiento.setEspacios_disponibles(espaciosD);
 			esService.insert(estacionamiento);
 		}else {
-			flash.addFlashAttribute("error", "El valor debe ser mayor a 0 y menor a "+estacionamiento.getEspacios());
+			flash.addFlashAttribute("error", "El valor debe ser > 0 y <= "+estacionamiento.getEspacios());
 		}
+		if(anticipacionDias>=0) {
+			estacionamiento.setAnticipacionDias(anticipacionDias);
+			esService.insert(estacionamiento);
+		}else {
+			flash.addFlashAttribute("error2", "El valor debe ser >= 0");
+		}
+		
+		
+		if(anticipacionHoras >= 1 && anticipacionHoras <= 12) {
+			estacionamiento.setAnticipacionHoras(anticipacionHoras);
+			if(anticipacionDias > 0) {
+				estacionamiento.setAnticipacionHoras(0);
+			}
+			esService.insert(estacionamiento);
+		}else {
+			if(anticipacionDias == 0) {
+				flash.addFlashAttribute("error3", "El valor debe ser entre 1 a 12");
+			}
+			
+		}
+		int numeroHoraApertura = Integer.parseInt(horaApertura.substring(0,1)); 
+		int numeroHoraCierre = Integer.parseInt(horaCierre.substring(0,1));
+		if(numeroHoraApertura < numeroHoraCierre) {
+			if(!horaApertura.equals("")) {
+				estacionamiento.setHoraApertura(horaApertura);
+				esService.insert(estacionamiento);
+			}else {
+				flash.addFlashAttribute("error4", "Error en la hora de apertura");
+			}
+			if(!horaCierre.equals("")) {
+				estacionamiento.setHoraCierre(horaCierre);
+				esService.insert(estacionamiento);
+			}else {
+				flash.addFlashAttribute("error5", "Error en la hora de cierre");
+			}
+		}else {
+			flash.addFlashAttribute("error6", "La hora de cierre no puede ser menor o igual a la hora de apertura");
+		}
+		
 		
 		return "redirect:/home";
 	}
