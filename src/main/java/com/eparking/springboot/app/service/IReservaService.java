3737869b40376e72ec95a7533d6d6d818deaf02a
CbdStl <71566917+CbdStl@users.noreply.github.com>
2020-11-11 23:17:53 -0500

    ServicioReserva

---
+++ src/main/java/com/eparking/springboot/app/service/IReservaService.java
@@ -0,0 +1,18 @@
+package com.eparking.springboot.app.service;
+
+import java.util.List;
+
+import com.eparking.springboot.app.entity.Reserva;
+import com.eparking.springboot.app.entity.Usuario;
+
+public interface IReservaService {
+	public void insert(Reserva reserva);
+	public void delete(Integer idReserva);
+	List<Reserva> list();
+	public Reserva findOne(Integer idReserva);
+	public List<Reserva> listByUserVehiculo(Usuario usuario);
+	public List<Reserva> listByUserEstacionamiento(Usuario usuario);
+	public List<Reserva> listByUserEstacionamientoHistorial(Usuario usuario);
+	public List<Reserva> listByUserVehiculoHistorial(Usuario usuario);
+
+}
