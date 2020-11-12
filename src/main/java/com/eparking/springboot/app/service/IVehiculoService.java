48a5c7221b79a887a256d702205d614b8f9559a9
CbdStl <71566917+CbdStl@users.noreply.github.com>
2020-11-11 22:36:45 -0500

    ServicioVehiculo

---
+++ src/main/java/com/eparking/springboot/app/service/IVehiculoService.java
@@ -0,0 +1,14 @@
+package com.eparking.springboot.app.service;
+
+import java.util.List;
+
+import com.eparking.springboot.app.entity.Usuario;
+import com.eparking.springboot.app.entity.Vehiculo;
+
+public interface IVehiculoService {
+	public void insert(Vehiculo vehiculo);
+	public void delete(Integer idVehiculo);
+	List<Vehiculo> list();
+	public Vehiculo findOne(Integer idVehiculo);
+	List<Vehiculo> listByUser(Usuario usuario);
+}
