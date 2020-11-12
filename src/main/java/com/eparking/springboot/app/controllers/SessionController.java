b372393de8a80a185c1b5f5b40840d14c81f880f
KazuM5 <67279238+KazuM5@users.noreply.github.com>
2020-11-11 20:01:15 -0500

    Add files via upload

---
+++ src/main/java/com/eparking/springboot/app/controllers/SessionController.java
@@ -0,0 +1,69 @@
+package com.eparking.springboot.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.eparking.springboot.app.entity.Estacionamiento;
+import com.eparking.springboot.app.entity.Usuario;
+import com.eparking.springboot.app.service.IEstacionamientoService;
+import com.eparking.springboot.app.service.IUsuarioService;
+
+@Controller
+@SessionAttributes("usuarioSesion")
+public class SessionController {
+	@Autowired
+	private IUsuarioService usService;
+	
+	@Autowired
+	private IEstacionamientoService esService;
+	
+	@GetMapping("/login")
+	public String index(SessionStatus status) {
+		status.setComplete();
+		return "login";
+	}
+	
+	@PostMapping("/login/post")
+	public String IniciarSesion(Usuario usuario, Model model) {
+		Usuario us = usService.login(usuario.getUsername(), usuario.getClave());
+		model.addAttribute("usuarioSesion", us);
+		if(us != null) {
+			return "redirect:/home";
+		}
+		model.addAttribute("error","Error al iniciar sesion "+usuario.getUsername() + " " + usuario.getClave());
+		return "login";
+	}
+	
+	@GetMapping("/logout")
+	public String CerrarSesion() {
+		return "redirect:/login";
+	}
+	
+	@RequestMapping("/cuenta")
+	public String cuenta() {
+		return "cuenta";
+	}
+	@RequestMapping("/estacionamiento")
+	public String estacionamiento(@ModelAttribute(name = "usuarioSesion") Usuario usuario, Model model, RedirectAttributes flash) {
+		if(usuario.getTipo().equals("C")) {
+			flash.addFlashAttribute("permiso", "administrador");
+			return "redirect:/home";
+		}
+		String pagina = "";
+		if(usuario.getTipo().equals("A")) {
+			Estacionamiento estacionamiento =  new Estacionamiento();
+			estacionamiento = esService.findByUser(usuario);
+			model.addAttribute("estacionamiento", estacionamiento);
+			pagina = "administrador/estacionamiento";
+		}
+		return pagina;
+	}
+}
---
+++ src/main/java/com/eparking/springboot/app/controllers/UsuarioController.java
@@ -0,0 +1,70 @@
+package com.eparking.springboot.app.controllers;
+
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.eparking.springboot.app.entity.Estacionamiento;
+import com.eparking.springboot.app.entity.Usuario;
+import com.eparking.springboot.app.service.IDistritoService;
+import com.eparking.springboot.app.service.IEstacionamientoService;
+import com.eparking.springboot.app.service.IUsuarioService;
+
+@Controller
+public class UsuarioController {
+	@Autowired
+	private IUsuarioService usService;
+	
+	@Autowired 
+	private IEstacionamientoService esService;
+	
+	@Autowired
+	private IDistritoService diService;
+	
+	@GetMapping("/registroCliente")
+	public String registroCliente(Model model) {
+		model.addAttribute("usuario", new Usuario());
+		return "cliente/registro";
+	}
+	
+	@GetMapping("/registroEstacionamiento")
+	public String registroEstacionamiento(Model model) {
+		model.addAttribute("estacionamiento", new Estacionamiento());
+		model.addAttribute("listDistritos", diService.list());
+		return "administrador/registro";
+	}
+	@PostMapping("/registro/save")
+	public String save(Usuario usuario, Model model,@RequestParam("imageFile") MultipartFile imageFile) throws Exception {
+		model.addAttribute("mensaje", "Se guardo correctamente el usuario");
+		usuario.setImagen(imageFile.getOriginalFilename());
+		usService.insert(usuario,imageFile);
+		return "redirect:/login";
+	}
+	
+	@PostMapping("/registroEstacionamiento/save") 
+	public String saveEstacionamiento(@Valid Estacionamiento estacionamiento, BindingResult result, Model model,@RequestParam(name = "imageFile",required = false) MultipartFile imageFile) throws Exception {
+		if(result.hasErrors()) {
+			return "administrador/registro";
+		}else {
+			model.addAttribute("mensaje", "Se guardo correctamente el usuario y estacionamiento");
+			estacionamiento.getUsuario().setTipo("A");
+			estacionamiento.setEspacios_disponibles(estacionamiento.getEspacios());
+			if(!imageFile.getOriginalFilename().equals("")) {
+				estacionamiento.getUsuario().setImagen(imageFile.getOriginalFilename());
+				esService.insertWithImage(estacionamiento, imageFile);
+			}else {
+				esService.insert(estacionamiento);
+			}
+			
+		}
+		return "redirect:/login";
+	}
+}
